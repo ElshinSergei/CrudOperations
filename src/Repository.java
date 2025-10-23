5620a5b0ec1f334b6271ed0811369b39a7c8961f
Elshin Sergei <elshin.sergei@list.ru>
2025-10-23 19:42:02 +0500

    FINISH

--- src/Repository.java
+++ src/Repository.java
@@ -1,8 +1,233 @@
-public class CreateStudent {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-    String sqlQuery = "jdbc:mysql://localhost:3306/?user=root";
-    String username = "root";
-    String password = "root";
+public class Repository {
+    static String url = "jdbc:mysql://localhost:3306/university";
+    static String username = "root";
+    static String password = "root";
 
+    public static void createStudent(Student student) {
+        try {
+            try (Connection connection = DriverManager.getConnection(url, username, password)) {
+                String sql = "INSERT INTO Students (name, surname, age, phone, email) VALUES(?, ?, ?, ?, ?)";
+                try (PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                    preparedStatement.setString(1, student.getName());
+                    preparedStatement.setString(2, student.getSurname());
+                    preparedStatement.setInt(3, student.getAge());
+                    preparedStatement.setString(4, student.getPhone());
+                    preparedStatement.setString(5, student.getEmail());
+                    preparedStatement.executeUpdate();
+//                    System.out.println("Good connection");
+                    try (ResultSet key = preparedStatement.getGeneratedKeys()) {
+                        if (key.next()) {
+                            int id = key.getInt(1);
+                            student.setId(id);
+                        }
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Bad connection");
+        }
+        System.out.println("Студент добавлен");
+    }
 
+    public static List<Student> reedAll() {
+        List<Student> students = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            String sql = "SELECT * FROM Students";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    Student student = new Student(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("surname"),
+                            resultSet.getInt("age"),
+                            resultSet.getString("phone"),
+                            resultSet.getString("email"));
+                    students.add(student);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Bad connection");
+        }
+        return students;
+    }
+
+    public static Student searchById(int numberId) {
+              Student student = null;
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            String sql = "SELECT * FROM Students WHERE id = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setInt(1, numberId);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                        student = new Student(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("surname"),
+                            resultSet.getInt("age"),
+                            resultSet.getString("phone"),
+                            resultSet.getString("email"));
+                    System.out.println("Студент найден!");
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Bad connection");
+        }
+        return student;
+    }
+
+    public static List<Student> searchByNameSurname(String nameSearch, String surnameSearch) {
+                List<Student> students = new ArrayList<>();
+        try(Connection connection = DriverManager.getConnection(url, username, password)) {
+            String sql = "SELECT * FROM Students WHERE name = ? AND surname = ?";
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setString(1, nameSearch);
+                preparedStatement.setString(2, surnameSearch);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while(resultSet.next()) {
+                    Student student = new Student(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("surname"),
+                            resultSet.getInt("age"),
+                            resultSet.getString("phone"),
+                            resultSet.getString("email"));
+                    System.out.println("Студент найден!");
+                    students.add(student);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return students;
+    }
+
+    public static Student searchByEmail(String emailSearch) {
+               Student student = null;
+        try(Connection connection = DriverManager.getConnection(url, username, password)) {
+            String sql = "SELECT * FROM Students WHERE email = ?";
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setString(1, emailSearch);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                if(resultSet.next()) {
+                    student = new Student(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("surname"),
+                            resultSet.getInt("age"),
+                            resultSet.getString("phone"),
+                            resultSet.getString("email"));
+                         System.out.println("Студент найден!");
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return student;
+    }
+
+    public static List<Student> searchByAge(int ageSearch) {
+               List<Student> students = new ArrayList<>();
+        try(Connection connection = DriverManager.getConnection(url, username, password)) {
+            String sql = "SELECT * FROM Students WHERE age = ?";
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setInt(1, ageSearch);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    Student student = new Student(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("surname"),
+                            resultSet.getInt("age"),
+                            resultSet.getString("phone"),
+                            resultSet.getString("email"));
+                    System.out.println("Студент найден!");
+                    students.add(student);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return students;
+    }
+
+    public static void updateStudent(int updateId, String updateName, String updateSurname, int updateAge, String updatePhone, String updateEmail) {
+           try(Connection connection = DriverManager.getConnection(url, username, password)) {
+               String sql = "UPDATE Students SET name = ?, surname = ?, age = ?, phone = ?, email = ? WHERE id = ?";
+               try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                   preparedStatement.setString(1, updateName);
+                   preparedStatement.setString(2, updateSurname);
+                   preparedStatement.setInt(3, updateAge);
+                   preparedStatement.setString(4, updatePhone);
+                   preparedStatement.setString(5, updateEmail);
+                   preparedStatement.setInt(6, updateId);
+                   preparedStatement.executeUpdate();
+                   System.out.println("Данные студента обновлены");
+               }
+           } catch (SQLException e) {
+               throw new RuntimeException(e);
+           }
+    }
+
+    public static void deleteById(int idDelete) {
+        try(Connection connection = DriverManager.getConnection(url, username, password)) {
+            String sql = "DELETE FROM Students WHERE id = ?";
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setInt(1, idDelete);
+                preparedStatement.executeUpdate();
+                System.out.println("Студент удален");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void deleteAll() {
+        try(Connection connection = DriverManager.getConnection(url, username, password)) {
+            String sql = "DELETE FROM Students";
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.executeUpdate();
+                System.out.println("Все записи удалены");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean validationEmail(String email) {
+        try(Connection connection = DriverManager.getConnection(url, username, password)) {
+            String sql = "SELECT COUNT(id) FROM Students WHERE email = ?";
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setString(1, email);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                  return resultSet.getInt(1) > 0;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
+
+    public static boolean validationId(int id) {
+        try(Connection connection = DriverManager.getConnection(url, username, password)) {
+            String sql = "SELECT COUNT(id) FROM Students WHERE id = ?";
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setInt(1, id);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    return resultSet.getInt(1) > 0;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
 }
+
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,86 @@
-package PACKAGE_NAME;
 
 public class Student {
+    int id;
+    String name;
+    String surname;
+    int age;
+    String phone;
+    String email;
+
+    public Student(String name, String surname, int age, String phone, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Student(int id, String name, String surname, int age, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Студент: " +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + "\n";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
